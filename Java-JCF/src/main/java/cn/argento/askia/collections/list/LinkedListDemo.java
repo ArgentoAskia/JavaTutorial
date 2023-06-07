2a799475ba720ece43eb58a87d510c0515b72528
ArgentoAskia <askiaxiaoming@outlook.com>
2023-06-07 23:55:28 +0800

    5-Java_JCF = add: 添加LinkedListDemo

---
+++ Java-JCF/src/main/java/cn/argento/askia/collections/list/LinkedListDemo.java
@@ -0,0 +1,9 @@
+package cn.argento.askia.collections.list;
+
+import java.util.LinkedList;
+
+public class LinkedListDemo {
+    public static void main(String[] args) {
+        LinkedList<String> linkedList = new LinkedList<>();
+    }
+}
