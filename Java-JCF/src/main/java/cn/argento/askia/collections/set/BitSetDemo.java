0c5cf20959fcfc10cf52b31130ca78cd01b32c32
ArgentoAskia <askiaxiaoming@outlook.com>
2023-06-09 15:42:08 +0800

    10-Java_JCF = add: 添加各类Set集合Demo（未完成）

---
+++ Java-JCF/src/main/java/cn/argento/askia/collections/set/BitSetDemo.java
@@ -0,0 +1,16 @@
+package cn.argento.askia.collections.set;
+
+import java.util.BitSet;
+
+public class BitSetDemo {
+    private static BitSet bitSet = new BitSet();
+
+    public static void main(String[] args) {
+
+
+    }
+
+
+
+
+}
---
+++ Java-JCF/src/main/java/cn/argento/askia/collections/set/LinkedHashSetDemo.java
@@ -0,0 +1,4 @@
+package cn.argento.askia.collections.set;
+
+public class LinkedHashSetDemo {
+}
