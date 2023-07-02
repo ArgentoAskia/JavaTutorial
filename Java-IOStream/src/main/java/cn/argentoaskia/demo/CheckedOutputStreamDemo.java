46e7bf61503a915cf37f49b3ed230c5b59214b5e
ArgentoAskia <askiaxiaoming@outlook.com>
2023-07-02 17:27:24 +0800

    6-Java_IOStream = add: 添加CheckedOutputDemno类

---
+++ Java-IOStream/src/main/java/cn/argentoaskia/demo/CheckedOutputStreamDemo.java
@@ -0,0 +1,11 @@
+package cn.argentoaskia.demo;
+
+import java.util.zip.CRC32;
+import java.util.zip.CheckedOutputStream;
+
+public class CheckedOutputStreamDemo {
+    public static void main(String[] args) {
+        CRC32 crc32 = new CRC32();
+//        CheckedOutputStream checkedOutputStream = new CheckedOutputStream();
+    }
+}
