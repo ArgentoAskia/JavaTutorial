959b502290df188c90592fce1dcf5abcef86c80a
ArgentoAskia <askiaxiaoming@outlook.com>
2023-08-07 15:01:52 +0800

    7-Java_File = add: 相关资源

---
+++ Java-Test/src/main/java/cn/SpringBootApplicationStarter.java
@@ -0,0 +1,11 @@
+package cn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootApplicationStarter {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootApplicationStarter.class, args);
+    }
+}
