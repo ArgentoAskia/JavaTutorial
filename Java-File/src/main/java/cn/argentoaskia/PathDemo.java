3234a73b7f530e0e2a5aad801353365434ba2b0d
ArgentoAskia <askiaxiaoming@outlook.com>
2023-08-10 19:31:27 +0800

    9-Java_File = add: PathDemo

--- Java-File/src/main/java/cn/argentoaskia/PathDemo.java
+++ Java-File/src/main/java/cn/argentoaskia/PathDemo.java
@@ -2,10 +2,7 @@ package cn.argentoaskia;
 
 import java.io.File;
 import java.net.URI;
-import java.nio.file.FileSystem;
-import java.nio.file.FileSystems;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.nio.file.*;
 import java.util.Iterator;
 
 public class PathDemo {
